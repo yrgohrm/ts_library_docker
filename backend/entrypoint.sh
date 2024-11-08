b5173e1bdb59fefefd3d02329897bce5f5da0eb5
Hampus Ram <hampus.ram@educ.goteborg.se>
2024-11-08 13:32:53 +0100

    make it more fail safe

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# give mysql a bit more time to really start before trying to connect
+sleep 5
+
 # make sure to run with URL from the environment and to use both the standard
 # migrations and the test migrations in /opt/library/migration
 java -jar /opt/library/libraryapp-*.jar db.url="${JDBC_URL}" \
